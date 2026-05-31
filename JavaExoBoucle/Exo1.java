47412684c2922a22d4e3adba06c6f18ad356942a
Coumba25040 <coumbaba.ga@gmail.com>
2026-05-31 14:28:05 +0200

    Update comment in Exo1.java
    Removed the part of the comment that mentions counting odd numbers.

--- JavaExoBoucle/Exo1.java
+++ JavaExoBoucle/Exo1.java
@@ -1,4 +1,4 @@
-//L'objectif de cet exercice est de créer un programme qui demande à l'utilisateur un nombres et afficher si le nombre est pair ou impair, et compte le nombre de nombres impairs.
+//L'objectif de cet exercice est de créer un programme qui demande à l'utilisateur un nombres et afficher si le nombre est pair ou impair.
 
 
 import java.util.Scanner;
